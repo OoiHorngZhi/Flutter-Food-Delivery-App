3f742e82991a5ac15bbe3c19615f2220a4effd81
OoiHorngZhi <sky524walker@gmail.com>
2023-06-17 14:59:01 +0800

    Initial commit

---
+++ lib/screens/homeScreen.dart
@@ -0,0 +1,441 @@
+import 'package:flutter/material.dart';
+import 'package:monkey_app_demo/screens/moreScreen.dart';
+
+import '../const/colors.dart';
+import '../utils/helper.dart';
+import '../widgets/customNavBar.dart';
+import '../screens/individualItem.dart';
+import '../widgets/searchBar.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const routeName = "/homeScreen";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          SafeArea(
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Good morning Akila!",
+                          style: Helper.getTheme(context).headline5,
+                        ),
+                        Image.asset(Helper.getAssetName("cart.png", "virtual"))
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                    ),
+                    child: Text("Deilivering to"),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                    ),
+                    child: DropdownButtonHideUnderline(
+                      child: SizedBox(
+                        width: 250,
+                        child: DropdownButton(
+                          value: "current location",
+                          items: [
+                            DropdownMenuItem(
+                              child: Text("Current Location"),
+                              value: "current location",
+                            ),
+                            DropdownMenuItem(
+                              child: Text("choose Location"),
+                              value: "choosed location",
+                            ),
+                          ],
+                          icon: Image.asset(
+                            Helper.getAssetName(
+                                "dropdown_filled.png", "virtual"),
+                          ),
+                          style: Helper.getTheme(context).headline4,
+                          onChanged: (_) {},
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  SearchBar(
+                    title: "Search Food",
+                  ),
+
+
+                  SizedBox(
+                    height: 50,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Popular Restaurants",
+                          style: Helper.getTheme(context).headline5,
+                        ),
+                        TextButton(onPressed: () {
+                          Navigator.of(context)
+                              .pushReplacementNamed(MoreScreen.routeName);
+                        }, child: Text("View all"))
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  RestaurantCard(
+                    image: Image.asset(
+                      Helper.getAssetName("pizza2.jpg", "real"),
+                      fit: BoxFit.cover,
+                    ),
+                    name: "Minute by tuk tuk",
+                  ),
+                  RestaurantCard(
+                    image: Image.asset(
+                      Helper.getAssetName("breakfast.jpg", "real"),
+                      fit: BoxFit.cover,
+                    ),
+                    name: "Cafe de Noir",
+                  ),
+                  RestaurantCard(
+                    image: Image.asset(
+                      Helper.getAssetName("bakery.jpg", "real"),
+                      fit: BoxFit.cover,
+                    ),
+                    name: "Bakes by Tella",
+                  ),
+                  SizedBox(
+                    height: 100,
+                  ),
+
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+              bottom: 0,
+              left: 0,
+              child: CustomNavBar(
+                home: true,
+              )),
+        ],
+      ),
+    );
+  }
+}
+
+class RecentItemCard extends StatelessWidget {
+  const RecentItemCard({
+    Key key,
+    @required String name,
+    @required Image image,
+  })  : _name = name,
+        _image = image,
+        super(key: key);
+
+  final String _name;
+  final Image _image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Container(
+            width: 80,
+            height: 80,
+            child: _image,
+          ),
+        ),
+        SizedBox(
+          width: 10,
+        ),
+        Expanded(
+          child: Container(
+            height: 100,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  _name,
+                  style: Helper.getTheme(context)
+                      .headline4
+                      .copyWith(color: AppColor.primary),
+                ),
+                Row(
+                  children: [
+                    Text("Cafe"),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 5.0),
+                      child: Text(
+                        ".",
+                        style: TextStyle(
+                          color: AppColor.green,
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text("Western Food"),
+                    SizedBox(
+                      width: 20,
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Image.asset(
+                      Helper.getAssetName("star_filled.png", "virtual"),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      "4.9",
+                      style: TextStyle(
+                        color: AppColor.green,
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Text('(124) Ratings')
+                  ],
+                )
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class MostPopularCard extends StatelessWidget {
+  const MostPopularCard({
+    Key key,
+    @required String name,
+    @required Image image,
+  })  : _name = name,
+        _image = image,
+        super(key: key);
+
+  final String _name;
+  final Image _image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Container(
+            width: 300,
+            height: 200,
+            child: _image,
+          ),
+        ),
+        SizedBox(height: 10),
+        Text(
+          _name,
+          style: Helper.getTheme(context)
+              .headline4
+              .copyWith(color: AppColor.primary),
+        ),
+        Row(
+          children: [
+            Text("Cafe"),
+            SizedBox(
+              width: 5,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 5.0),
+              child: Text(
+                ".",
+                style: TextStyle(
+                  color: AppColor.green,
+                  fontWeight: FontWeight.w900,
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 5,
+            ),
+            Text("Western Food"),
+            SizedBox(
+              width: 20,
+            ),
+            Image.asset(
+              Helper.getAssetName("star_filled.png", "virtual"),
+            ),
+            SizedBox(
+              width: 5,
+            ),
+            Text(
+              "4.9",
+              style: TextStyle(
+                color: AppColor.green,
+              ),
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
+
+class RestaurantCard extends StatelessWidget {
+  const RestaurantCard({
+    Key key,
+    @required String name,
+    @required Image image,
+  })  : _image = image,
+        _name = name,
+        super(key: key);
+
+  final String _name;
+  final Image _image;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 270,
+      width: double.infinity,
+      child: Column(
+        children: [
+          SizedBox(height: 200, width: double.infinity, child: _image),
+          SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+            ),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      _name,
+                      style: Helper.getTheme(context).headline3,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Row(
+                  children: [
+                    Image.asset(
+                      Helper.getAssetName("star_filled.png", "virtual"),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      "4.9",
+                      style: TextStyle(
+                        color: AppColor.green,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text("(124 ratings)"),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text("Cafe"),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 5.0),
+                      child: Text(
+                        ".",
+                        style: TextStyle(
+                          color: AppColor.green,
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text("Western Food"),
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class CategoryCard extends StatelessWidget {
+  const CategoryCard({
+    Key key,
+    @required Image image,
+    @required String name,
+  })  : _image = image,
+        _name = name,
+        super(key: key);
+
+  final String _name;
+  final Image _image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Container(
+            width: 100,
+            height: 100,
+            child: _image,
+          ),
+        ),
+        SizedBox(
+          height: 5,
+        ),
+        Text(
+          _name,
+          style: Helper.getTheme(context)
+              .headline4
+              .copyWith(color: AppColor.primary, fontSize: 16),
+        ),
+      ],
+    );
+  }
+}
